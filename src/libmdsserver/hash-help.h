93028c5aad38947813f2d2a3b079b48514fe756d
Mattias Andrée <maandree@operamail.com>
2014-04-22 11:25:40 +0200

    add string hasher
    Signed-off-by: Mattias Andrée <maandree@operamail.com>

---
+++ src/libmdsserver/hash-help.h
@@ -0,0 +1,44 @@
+/**
+ * mds — A micro-display server
+ * Copyright © 2014  Mattias Andrée (maandree@member.fsf.org)
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef MDS_LIBMDSSERVER_HASH_HELP_H
+#define MDS_LIBMDSSERVER_HASH_HELP_H
+
+
+#include <stdlib.h>
+
+
+/**
+ * Calculate the hash of a string
+ * 
+ * @param   str  The string
+ * @return         The hash of the string
+ */
+static inline size_t __attribute__((const)) string_hash(const char* str)
+{
+  size_t hash = 0;
+  
+  if (str != NULL)
+    while (*str != '\0')
+      hash = hash * 31 + (size_t)(unsigned char)*str++;
+  
+  return hash;
+}
+
+
+#endif
+
